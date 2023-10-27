0d003dbfabdd9228784700c88416a43cc5b6d47c
Vinicius Gomes <vinigomes.0311@gmail.com>
2023-10-26 22:10:46 -0300

    feature: add route for donwload finished videos

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,12 @@
 import cors from 'cors'
-import express from 'express'
+import express, { Request, Response } from 'express'
 import { Server } from 'http'
 import { v4 as uuid } from 'uuid'
 import { videoGenerator } from './services'
 import { initializeSocket } from './socket'
+
+import fs from 'node:fs'
+import path from 'node:path'
 
 const app = express()
 const PORT = 10000
@@ -21,7 +24,7 @@ initializeSocket(httpServer)
 
 app.use(express.json())
 
-app.post('/generate', async (req, res) => {
+app.post('/generate', async (req: Request, res: Response) => {
   const { feature, programming_language } = req.body
 
   const id = uuid()
@@ -29,6 +32,21 @@ app.post('/generate', async (req, res) => {
   videoGenerator(feature, programming_language, id)
 
   res.status(200).json({ video_id: id })
+})
+
+app.get('/download/:id', (req: Request, res: Response) => {
+  const { id } = req.params
+
+  const videoFilePath = path.join(__dirname, '..', `results/${id}/output_final_video.mp4`)
+  if (!fs.existsSync(videoFilePath)) {
+    return res.status(404).send('Video not found')
+  }
+
+  res.setHeader('Content-Type', 'video/mp4')
+  res.setHeader('Content-Disposition', `attachment; filename=${id}.mp4`)
+
+  const stream = fs.createReadStream(videoFilePath)
+  stream.pipe(res)
 })
 
 httpServer.listen(PORT, () => {
