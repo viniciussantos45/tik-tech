671fd978c514a1a9860d3c85262be3442850b915
Vinicius Gomes <vinigomes.0311@gmail.com>
2023-10-31 22:49:24 -0300

    refactor: build video command for best performance

--- src/services/BuildVideo.ts
+++ src/services/BuildVideo.ts
@@ -1,6 +1,5 @@
 import { exec as execCallback } from 'child_process'
 
-import fs from 'node:fs/promises'
 import path from 'node:path'
 import { promisify } from 'util'
 import { getRandomInt } from '../utils'
@@ -9,50 +8,62 @@ const exec = promisify(execCallback)
 
 const PATH_RESULTS = process.env.PATH_RESULTS ?? 'results'
 
+const getMediaDuration = async (filePath: string) => {
+  const { stdout } = await exec(
+    `ffprobe -v error -show_entries format=duration -of default=noprint_wrappers=1:nokey=1 "${filePath}"`
+  )
+  return parseFloat(stdout.trim())
+}
+
 export const buildVideo = async (id: string) => {
   const videoBase: string = path.resolve(__dirname, `../../videos/${getRandomInt(1, 14)}.mp4`)
   const audio: string = path.resolve(process.cwd(), `${PATH_RESULTS}/${id}/audio.mp3`)
   const imagem: string = path.resolve(process.cwd(), `${PATH_RESULTS}/${id}/code.png`)
-  const legenda: string = path.resolve(process.cwd(), `${PATH_RESULTS}/${id}/captions.ass`) // problema de caminho absoluto
+  const legenda: string = path.resolve(process.cwd(), `${PATH_RESULTS}/${id}/captions.ass`)
   const cover: string = path.resolve(process.cwd(), `${PATH_RESULTS}/${id}/cover.png`)
 
-  const outputSegment: string = path.resolve(process.cwd(), `${PATH_RESULTS}/${id}/output_segment.mp4`)
-  const outputVideoCover: string = path.resolve(process.cwd(), `${PATH_RESULTS}/${id}/output_cover.mp4`)
-  const outputWithAudio: string = path.resolve(process.cwd(), `${PATH_RESULTS}/${id}/output_with_audio.mp4`)
-  const outputWithImageCode: string = path.resolve(process.cwd(), `${PATH_RESULTS}/${id}/output_with_image.mp4`)
-  const outputWithBlur: string = path.resolve(process.cwd(), `${PATH_RESULTS}/${id}/output_with_blur.mp4`)
-  const outputResized: string = path.resolve(process.cwd(), `${PATH_RESULTS}/${id}/output_resized.mp4`)
-  const outputPrincipalVideo: string = path.resolve(process.cwd(), `${PATH_RESULTS}/${id}/output_principal_video.mp4`)
   const outputFinalVideo: string = path.resolve(process.cwd(), `${PATH_RESULTS}/${id}/output_final_video.mp4`)
 
-  const commands = [
-    // Make cover
-    `ffmpeg -y -i ${videoBase} -vf "scale=1080:1920:force_original_aspect_ratio=increase,crop=1080:1920" -t 2 ${outputSegment}`,
-    `ffmpeg -y -i ${outputSegment} -i ${cover} -filter_complex "[0:v][1:v] overlay=(main_w-overlay_w)/2:(main_h-overlay_h)/2" ${outputVideoCover}`,
-    // Make principal video
-    `ffmpeg -y -i ${videoBase} -vf "scale=1080:1920:force_original_aspect_ratio=increase,crop=1080:1920" ${outputResized}`,
-    `ffmpeg -y -i ${outputResized} -vf "boxblur=5:5" ${outputWithBlur}`,
-    `ffmpeg -y -stream_loop -1 -i ${outputWithBlur} -i ${audio} -shortest -map 0:v:0 -map 1:a:0 ${outputWithAudio}`,
+  const audioDuration = await getMediaDuration(audio)
+  const videoBaseDuration = await getMediaDuration(videoBase)
 
-    `ffmpeg -y -i ${outputWithAudio} -i ${imagem} -filter_complex "[0:v][1:v] overlay=(main_w-overlay_w)/2:(main_h-overlay_h)/2" ${outputWithImageCode}`,
+  const loopsRequired = Math.ceil(audioDuration / videoBaseDuration)
 
-    `ffmpeg -y -i ${outputWithImageCode} -vf "ass='${legenda
-      .replace(/\\/g, '\\\\')
-      .replace(':', '\\:')}'" ${outputPrincipalVideo}`,
+  const loopedVideoPath = path.resolve(process.cwd(), `${PATH_RESULTS}/${id}/looped_video.mp4`)
 
-    `ffmpeg -i ${outputVideoCover} -i ${outputPrincipalVideo} -filter_complex "[0:v:0][0:a:0][1:v:0][1:a:0]concat=n=2:v=1:a=1[outv][outa]" -map "[outv]" -map "[outa]" -vcodec libx264 -acodec aac ${outputFinalVideo}`
-  ]
+  const loopCommand = `
+    ffmpeg -y
+    -stream_loop ${loopsRequired - 1}
+    -i "${videoBase}"
+    -c copy
+    ${loopedVideoPath}
+  `.replace(/(\n)/g, '')
 
-  for (const command of commands) {
-    console.log(`execute command: `, command)
-    await exec(command)
-  }
+  await exec(loopCommand)
 
-  await fs.unlink(outputSegment)
-  await fs.unlink(outputVideoCover)
-  await fs.unlink(outputWithAudio)
-  await fs.unlink(outputWithImageCode)
-  await fs.unlink(outputWithBlur)
-  await fs.unlink(outputResized)
-  await fs.unlink(outputPrincipalVideo)
+  const complexFilter = `
+    [0:v]scale=1080:1920:force_original_aspect_ratio=increase,crop=1080:1920,trim=0:${audioDuration}[tocover];
+    [tocover]boxblur=5:5[blurred];
+    [blurred][3:v]overlay=(main_w-overlay_w)/2:(main_h-overlay_h)/2:enable='between(t,0,1)'[coverOverlay];
+    [coverOverlay][2:v]overlay=(main_w-overlay_w)/2:(main_h-overlay_h)/2:enable='gte(t,1.1)'[vidWithImage];
+    [vidWithImage]ass='${legenda.replace(/\\/g, '\\\\').replace(':', '\\:')}',setpts=PTS+2/TB[outv];
+    [1:a]adelay=2000|2000,aformat=sample_fmts=fltp:sample_rates=44100:channel_layouts=stereo,aresample=async=1:first_pts=0[finalAudio]
+`.replace(/(\n)/g, '')
+
+  const command = `
+    ffmpeg -y
+    -i "${loopedVideoPath}"
+    -i "${audio}"
+    -i "${imagem}"
+    -i "${cover}"
+    -filter_complex "${complexFilter.trim()}"
+    -map "[outv]"
+    -map "[finalAudio]"
+    -vcodec libx264
+    -preset ultrafast
+    -acodec aac
+    ${outputFinalVideo}
+`.replace(/(\n)/g, '')
+
+  await exec(command)
 }
